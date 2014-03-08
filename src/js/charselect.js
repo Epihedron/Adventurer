a434771f4150494345bbe54685426bae500afe3b
Alexander Kindzerske <akindzerske@gmail.com>
2014-03-08 00:20:35 -0500

    Completed view level edit/checkbox behavior, need to add delete option

--- src/js/charselect.js
+++ src/js/charselect.js
@@ -1,55 +1,56 @@
 var user;
 //logincheck
-$.ajax({
-    url:'../php/init.php',
-    type:'post',
-    data:{'userquery':1},
-    success:function(data)
-        {
-            user = data;
-            if(user == '')
-            {
-                alert('YOU MUST LOG IN!');
-                window.location.replace('../../index.html');
-            }
-            else
-            {
-                $('#user').text(user);
-            }
-        }
-    }
-);
+// $.ajax({
+//     url:'../php/init.php',
+//     type:'post',
+//     data:{'userquery':1},
+//     success:function(data)
+//         {
+//             user = data;
+//             if(user == '')
+//             {
+//                 alert('YOU MUST LOG IN!');
+//                 window.location.replace('../../index.html');
+//             }
+//             else
+//             {
+//                 $('#user').text(user);
+//             }
+//         }
+//     }
+// );
 
-//ajax for character list
-$.ajax({
-	url:'../php/init.php',
-	type:'post',
-	data:{'charlistquery':1},
-	success:function(data)
-	{
-		var d = JSON.parse(data);
-		for(var i in d)
-		{
-			$("#charlist").append("<li value="+i+">"+d[i]+"</li>");
-		}
+// //ajax for character list
+// $.ajax({
+// 	url:'../php/init.php',
+// 	type:'post',
+// 	data:{'charlistquery':1},
+// 	success:function(data)
+// 	{
+// 		var d = JSON.parse(data);
+// 		for(var i in d)
+// 		{
+// 			// $("#charlist").append('<li value='+i+'>'+d[i]+'</li>'); previous list formatting
+//             $("#charlist").append('<li value='+i+'><input id="cb'+i+'" name="cb'+i+'" type="checkbox"><label for="cb'+i+'"><span></span><span>'+d[i]+'</span><span></span></label></li>');
+// 		}
 
-		//char selected
-		$('#charlist li').click(function()
-		{
-			$.ajax(
-			{
-				url:'../php/init.php',
-				type:'POST',
-				data:{cc:d[this.value]},
-				success:function(data)
-				{
-					data='';
-				}
-			});
-			setTimeout("window.location.href='../html/charactersheet.html?char="+new Date().getTime()+"';",300);
-		});
-	}
-});
+// 		//char selected
+// 		$('#charlist li').click(function()
+// 		{
+// 			$.ajax(
+// 			{
+// 				url:'../php/init.php',
+// 				type:'POST',
+// 				data:{cc:d[this.value]},
+// 				success:function(data)
+// 				{
+// 					data='';
+// 				}
+// 			});
+// 			setTimeout("window.location.href='../html/charactersheet.html?char="+new Date().getTime()+"';",300);
+// 		});
+// 	}
+// });
 
 //basic menu button items
 $("#tomain").click(function()
@@ -68,37 +69,48 @@ $("#newchar").click(function()
     }
 );
 
-//delete character
-$("#delchar").click(function()
-{
-    var p1=prompt("Enter character's name(CaSe SeNsItIvE):");
-    if(p1 != null)
-        {
-            var p2=prompt("Measure twice, cut once:");
-            if(p2 != null)
-                {
-                    if(p1==p2)
-                        {
-                            $.ajax(
-                                {
-                                    url:"../php/init.php",
-                                    type:"POST",
-                                    data:{"delchar":p2},
-                                    success:function(data)
-                                    {
-                                        alert(data);
-                                    },
-                                    error:function(err)
-                                    {
-                                        alert("An error was thrown:"+JSON.stringify(err));
-                                    }
-                                }
-                            ).done(function(){location.reload(true);});
-                        }
-                    else
-                        {
-                            alert('The names did not match.');
-                        }
-                }
-        }
+//hide/reveal character check boxes
+// $('#visibility-toggle').click(function(){
+//   if ( $('.me-select label::before').css('visibility') == 'hidden' )
+//     $('.me-select label::before').css('visibility','visible');
+//   else
+//     $('.me-select label::before').css('visibility','hidden');
+// });
+$("#visibility-toggle").click(function(){
+    $("input[type=checkbox], #checkbox").fadeToggle(200);
 });
+
+//delete character
+// $("#delchar").click(function()
+// {
+//     var p1=prompt("Enter character's name(CaSe SeNsItIvE):");
+//     if(p1 != null)
+//         {
+//             var p2=prompt("Measure twice, cut once:");
+//             if(p2 != null)
+//                 {
+//                     if(p1==p2)
+//                         {
+//                             $.ajax(
+//                                 {
+//                                     url:"../php/init.php",
+//                                     type:"POST",
+//                                     data:{"delchar":p2},
+//                                     success:function(data)
+//                                     {
+//                                         alert(data);
+//                                     },
+//                                     error:function(err)
+//                                     {
+//                                         alert("An error was thrown:"+JSON.stringify(err));
+//                                     }
+//                                 }
+//                             ).done(function(){location.reload(true);});
+//                         }
+//                     else
+//                         {
+//                             alert('The names did not match.');
+//                         }
+//                 }
+//         }
+// });
--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,45 @@
 var today;
-$.ajax({
-	url:'../php/init.php',
-	type:'post',
-	data:{userquery:1},
-	success:function(data)
-	{
-		if(data=="")
-		{
-			alert('You must login');
-			setTimeout(function()
-				{
-					window.location.href='../../index.html';
-				}
-			);
-		}
-		else
-		{
-			//the date
-			function makeArray() 
-			{
-				for (i = 0; i<makeArray.arguments.length; i++)
-				this[i + 1] = makeArray.arguments[i];
-			}
+// $.ajax({
+// 	url:'../php/init.php',
+// 	type:'post',
+// 	data:{userquery:1},
+// 	success:function(data)
+// 	{
+// 		if(data=="")
+// 		{
+// 			alert('You must login');
+// 			setTimeout(function()
+// 				{
+// 					window.location.href='../../index.html';
+// 				}
+// 			);
+// 		}
+// 		else
+// 		{
+// 			//the date
+// 			function makeArray() 
+// 			{
+// 				for (i = 0; i<makeArray.arguments.length; i++)
+// 				this[i + 1] = makeArray.arguments[i];
+// 			}
 
-			var months = new makeArray('January','February','March','April','May',
-			'June','July','August','September','October','November','December');
-			var date = new Date();
-			var day = date.getDate();
-			var month = date.getMonth() + 1;
-			var yy = date.getYear();
-			var year = (yy < 1000) ? yy + 1900 : yy;
-			$("#user").text(data);
-			$("#date").text(day + " " + months[month] + " " + year);
-		}
-	},
-	error:function()
-	{
-		alert('ajax encounter a problem');
-		window.location.href = '../index.html';
-	}
-});
+// 			var months = new makeArray('January','February','March','April','May',
+// 			'June','July','August','September','October','November','December');
+// 			var date = new Date();
+// 			var day = date.getDate();
+// 			var month = date.getMonth() + 1;
+// 			var yy = date.getYear();
+// 			var year = (yy < 1000) ? yy + 1900 : yy;
+// 			$("#user").text(data);
+// 			$("#date").text(day + " " + months[month] + " " + year);
+// 		}
+// 	},
+// 	error:function()
+// 	{
+// 		alert('ajax encounter a problem');
+// 		window.location.href = '../index.html';
+// 	}
+// });
 $("#forums").click(function()
 	{
 		window.location.href = 'http://numaria.createaforum.com';
