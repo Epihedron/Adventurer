969e27563e4974783d7145a8ca9d5df7757ef43c
13ruce1337 <tagcoop@gmail.com>
2014-03-08 11:41:54 -0500

    still working on new pdo

--- src/php/database.php
+++ src/php/database.php
@@ -1,8 +1,16 @@
 <?php
+	
+	//const db = new PDO('mysql:host=localhost;dbname=adventurer','host','');
+	
+	//public $user = 'ok';
+	//$char = $_SESSION['char'];
 
-class dbConnect
-{
-	const db = new PDO('mysql:host=localhost;dbname=adventurer','host','');
-}
+	//public function userquery() {
+	//	global $user;
+	//	echo $user;
+	//	return false;
+	//}
+
+echo 'kk';
 
 ?>
