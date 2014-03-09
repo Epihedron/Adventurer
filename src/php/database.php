505ef8644c3f3bd033d3223a87f94f4b138fe423
13ruce1337 <tagcoop@gmail.com>
2014-03-09 01:27:25 -0500

    login now has PDO

--- src/php/database.php
+++ src/php/database.php
@@ -5,6 +5,22 @@ $db = new PDO("mysql:host=localhost;dbname=adventurer",'host','');
 
 //class for basic single model query
 class dbQ {
+	//login query
+	function loginQ($user,$password) {
+		global $db;
+
+		$q = "select * from accounts where username = :user";
+		$stmt = $db->prepare($q);
+		$stmt->execute(array('user' => $user));
+		$r = $stmt->fetch(); 
+		if($user == $r['username'] && $password == $r['password']) {
+			$_SESSION['user'] = $user;
+			header("LOCATION:../html/main.html");
+		} else {
+			echo("incorrect login info");
+			echo("<meta http-equiv='refresh' content='2;../../index.html'/>");
+		}
+	}
 	
 	//user query
 	function userQ() {
