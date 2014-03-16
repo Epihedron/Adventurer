1eebf4b69e2a5dd3f683ef44c66d42526ba9730e
13ruce1337 <tagcoop@gmail.com>
2014-03-16 15:23:44 -0400

    cleaned up init.php once again. i want to get it so people can easily write their own controllers

--- src/php/init.php
+++ src/php/init.php
@@ -1,35 +1,39 @@
 <?php
-	require('database.php');
+require('database.php');
 
-	//calling in the database controller
-	$dbQ = new dbQ;
-	$dbD = new dbD;
+//database controller classes
+$dbQ = new dbQ;
+$dbD = new dbD;
 
-	//session calls
-	session_start();
-	$user=$_SESSION['user'];
-	$char = (isset($_SESSION['character']) ? $_SESSION['character'] : null);
+//php client session variables and start
+session_start();
+$user = $_SESSION['user'];
+$char = (isset($_SESSION['character']) ? $_SESSION['character'] : null);
 
-	//handles for database controller
-	//basic queries
-	$userquery = (isset($_POST['userquery']) ? $dbQ->userQ() : false); 
-   	$charquery = (isset($_POST['charquery']) ? $dbQ->charQ() : false); 
-	$charlistquery = (isset($_POST['charlistquery']) ? $dbQ->charlistQ() : false);
-	$charstatsquery = (isset($_POST['basicstats']) ? $dbQ->singleQ('*','characters') : false);
-	$powersquery = (isset($_POST['powers']) ? $dbQ->multiQ('*','powers') : false);
-	$cfeaturesquery = (isset($_POST['cfeatures']) ? $dbQ->multiQ('*','cfeatures') : false);
-	$rfeaturesquery = (isset($_POST['rfeatures']) ? $dbQ->multiQ('*','rfeatures') : false);
-	$featsquery = (isset($_POST['feats']) ? $dbQ->multiQ('*','feats') : false);
-	$languagesquery = (isset($_POST['languages']) ? $dbQ->multiQ('*','languages') : false);
-	$inventoryquery = (isset($_POST['inventory']) ? $dbQ->multiQ('*','inventory') : false);
-	$wealthquery = (isset($_POST['wealth']) ? $dbQ->singleQ('*','wealth') : false);
+/** 
+	ajax handles for the database controller
+	each one is listed by their names
+**/
 
-	//update queries
-	$charchange = (isset($_POST['cc']) ? $_SESSION['character']=$_POST['cc'] : false);
+//basic queries
+$userquery = (isset($_POST['userquery']) ? $dbQ->userQ() : false); 
+$charquery = (isset($_POST['charquery']) ? $dbQ->charQ() : false); 
+$charlistquery = (isset($_POST['charlistquery']) ? $dbQ->charlistQ() : false);
+$charstatsquery = (isset($_POST['basicstats']) ? $dbQ->singleQ('*','characters') : false);
+$powersquery = (isset($_POST['powers']) ? $dbQ->multiQ('*','powers') : false);
+$cfeaturesquery = (isset($_POST['cfeatures']) ? $dbQ->multiQ('*','cfeatures') : false);
+$rfeaturesquery = (isset($_POST['rfeatures']) ? $dbQ->multiQ('*','rfeatures') : false);
+$featsquery = (isset($_POST['feats']) ? $dbQ->multiQ('*','feats') : false);
+$languagesquery = (isset($_POST['languages']) ? $dbQ->multiQ('*','languages') : false);
+$inventoryquery = (isset($_POST['inventory']) ? $dbQ->multiQ('*','inventory') : false);
+$wealthquery = (isset($_POST['wealth']) ? $dbQ->singleQ('*','wealth') : false);
 
-	//delete queries
-	$deletechar = (isset($_POST['delchar']) ? $dbD->charD($_POST['delchar'],$user) : false);
-	
-	//new value queries
-	$newcharvalue = (isset($_POST['newval']) && isset($_POST['newtype']) ? $dbQ->ncharvalQ($_POST['newval'],$_POST['newtype']) : false);
+//update queries
+$charchange = (isset($_POST['cc']) ? $_SESSION['character']=$_POST['cc'] : false);
+
+//delete queries
+$deletechar = (isset($_POST['delchar']) ? $dbD->charD($_POST['delchar'],$user) : false);
+
+//new value queries
+$newcharvalue = (isset($_POST['newval']) && isset($_POST['newtype']) ? $dbQ->ncharvalQ($_POST['newval'],$_POST['newtype']) : false);
 ?>
